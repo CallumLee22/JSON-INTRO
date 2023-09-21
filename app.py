17726ed989fda27f00f2f9c3b5f4c392bfd0ac00
Callum Lee <callum.lee@stfc.ac.uk>
2023-09-21 15:44:13 +0000

    hierarchy json filed used to print name of file

--- app.py
+++ app.py
@@ -1,25 +1,6 @@
 import json
 
-with open("data.json") as f:
-    data = json.load(f)
+with open("hierarchy.json") as f: 
+    hierarchy = json.load(f)
 
-for states in data["states"]:
-    print(states["name"])
-
-for states in data["states"]:
-    del states["area_codes"]
-
-with open("no_area_code_states.json", "w") as f:
-    json.dump(data, f, indent=2)
-
-for states in data["states"]:
-    del states["name"]
-
-with open("no_name_states.json", "w") as f:
-    json.dump(data, f, indent=2)
-
-for states in data["states"]:
-    del states["abbreviation"]
-
-with open("no_abbreviation_states.json", "w") as f:
-    json.dump(data, f, indent=2)
+print(hierarchy[0]["contents"][3]["name"])
